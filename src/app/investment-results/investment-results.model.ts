6c5f9841ec4757bf0dfd4ca03160b46b124fe0da
ZsoltZsupos <zsupos.zsolt98@gmail.com>
2025-03-11 16:38:10 +0100

    investment model added

---
+++ src/app/investment-results/investment-results.model.ts
@@ -0,0 +1,8 @@
+export interface InvestmentResults {
+    year: number,
+    interest: number,
+    valueEndOfYear: number,
+    annualInvestment: number,
+    totalInterest: number,
+    totalAmountInvested: number,
+  };
